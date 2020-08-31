7d0306217ae31b29d5c0d5a1dcf2b33f8eddd89e
Milan <milanstoj94@gmail.com>
2020-08-31 10:48:49 -0500

    initial commit

---
+++ WebAPISample/Controllers/MovieController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPISample.Data;
+using WebAPISample.Models;
+
+namespace WebAPISample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        private ApplicationContext _context;
+        public MovieController(ApplicationContext context)
+        {
+            _context = context;
+        }
+        // GET api/movie
+        [HttpGet]
+        public IActionResult Get()
+        {
+            // Retrieve all movies from db logic
+            return Ok(new string[] { "movie1 string", "movie2 string" });
+        }
+
+        // GET api/movie/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            // Retrieve movie by id from db logic
+            // return Ok(movie);
+            return Ok();
+        }
+
+        // POST api/movie
+        [HttpPost]
+        public IActionResult Post([FromBody]Movie value)
+        {
+            // Create movie in db logic
+            return Ok();
+        }
+
+        // PUT api/movie
+        [HttpPut]
+        public IActionResult Put([FromBody] Movie movie)
+        {
+            // Update movie in db logic
+            return Ok();
+        }
+
+        // DELETE api/movie/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            // Delete movie from db logic
+            return Ok();
+        }
+    }
+}
---
+++ WebAPISample/Data/ApplicationContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPISample.Models;
+
+namespace WebAPISample.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions options)
+            :base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Seed data - needs migration
+           
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
