33deb1ff57025a29200443655701cb799706ac01
Milan <milanstoj94@gmail.com>
2020-08-31 11:40:58 -0500

    seeded database with movies

--- WebAPISample/Data/ApplicationContext.cs
+++ WebAPISample/Data/ApplicationContext.cs
@@ -14,8 +14,14 @@ namespace WebAPISample.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            // Seed data - needs migration
-           
+            modelBuilder.Entity<Movie>()
+             .HasData(
+                new Movie { MovieId = 1, Title = "The Departed", Genre = "Drama", Director = "Martin Scorsese" },
+                new Movie { MovieId = 2, Title = "The Dark Knight", Genre = "Drama", Director = "Christopher Nolan" },
+                new Movie { MovieId = 3, Title = "Inception", Genre = "Drama", Director = "Christopher Nolan" },
+                new Movie { MovieId = 4, Title = "Pineapple Express", Genre = "Comedy", Director = "David Gordon Green" },
+                new Movie { MovieId = 5, Title = "Die Hard", Genre = "Action", Director = "John McTiernan" }
+             );
         }
 
         public DbSet<Movie> Movies { get; set; }
